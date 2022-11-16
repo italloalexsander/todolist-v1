98c28c7b7f294e6ee4316973d3b67e64a102f8da
italloalexsander <italloalexsander@gmail.com>
2022-11-16 20:13:01 -0300

    Add basic styling

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
 app.set("view engine", "ejs");
 
 let items = ["Buy Food", "Cook Food", "Eat Food"]
@@ -19,16 +21,16 @@ app.get("/", (req, res) => {
 
   let currentDay = today.toLocaleDateString("en-US", options);
 
-  res.render('list', {day: currentDay, newItems: items});
+  res.render('list', { day: currentDay, newItems: items });
 
 });
 
-app.post("/", (req, res)=>{
+app.post("/", (req, res) => {
   let item = req.body.newItem;
   items.push(item);
   res.redirect("/");
 })
 
-app.listen(3000, (req, res)=>{
+app.listen(3000, (req, res) => {
   console.log("Server started on port 3000");
 });
