4e24033c8e3d7e99bc9b75e3ff72958112a1d6ab
italloalexsander <italloalexsander@gmail.com>
2022-11-16 18:32:05 -0300

    Add ejs integration

--- app.js
+++ app.js
@@ -3,9 +3,20 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
+  if (currentDay === 6 || currentDay ===0){
+    day = "a Weekend";
+  }else{
+    day = "a Weekday";
+  }
+  res.render('list', {kindOfDay: day});
+
 });
 
 app.listen(3000, (req, res)=>{
